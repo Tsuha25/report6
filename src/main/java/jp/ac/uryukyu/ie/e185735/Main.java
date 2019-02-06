8eac493c3db9fa20860b051a79403e16c4f27109
Syoya Tsuha <e185735@ie.u-ryukyu.ac.jp>
2019-02-06 13:32:42 +0900

    2st Commit

--- src/main/java/jp/ac/uryukyu/ie/e185735/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185735/Main.java
@@ -1,7 +1,9 @@
 package jp.ac.uryukyu.ie.e185735;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        //System.out.println("Hello"); jarファイルを生成できているかを最初に確認するためのHello
+
+        RPG_Job job1 = new RPG_Job();
     }
 }
---
+++ src/main/java/jp/ac/uryukyu/ie/e185735/RPG_Job.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.e185735;
+
+public class RPG_Job {
+    String name;
+    int hitpoint;
+    int attack;
+    int magicattack;
+    int defense;
+    int magicdefense;
+
+    public RPG_Job(){
+        System.out.println("ジョブの作成をします。");
+    }
+}
